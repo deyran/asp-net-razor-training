25090fa2f13c07451d6b0b3e350bc71e0748a89e
Deyvid Rannyere <deyran@gmail.com>
2023-11-15 13:27:11 -0300

    update

---
+++ MyRazorApp/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
